671a31143d469f7382f694dccfd2d80f34e9e11d
John Ly <57511109+johnly83@users.noreply.github.com>
2022-05-24 14:12:44 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -10,14 +10,24 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
-
   var result = num1 + num2;
 
   res.send("The result of the addition calculation of 2 numbers: " + result);
 });
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight/(height*height);
+
+  res.send("Your BMI is " + bmi);
+})
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
